be17e7fc4d37e6a90ad3076f706bc21e246e5e3c
Dasha <dasha>
2022-04-04 13:53:04 +0300

    Занятие 14: домашнее задание

---
+++ src/ru/itmo/lessons/lesson14/hw/Employee.java
@@ -0,0 +1,34 @@
+package ru.itmo.lessons.lesson14.hw;
+
+// Создать список объектов List<Employee> (использовать метод employeeGenerator)
+// и сортировать по:
+// имени
+// имени и зарплате
+// имени, зарплате, возрасту и компании
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    private String name;
+    private String company;
+    private int salary;
+    private int age;
+
+    // TODO: конструктор, геттеры и сеттеры
+
+    public static List<Employee> employeeGenerator(int num) {
+        // метод для создания списка объектов класса Employee
+        String[] names = {"Mike", "Tom", "Alex", "John", "Peter", "Jack", "Charlie", "Max", "Jenifer", "Linda", "Elizabeth"}; // массив с именами
+        String[] companies = {"Microsoft", "IBM", "Google", "General Electric", "Siemens", "Samsung", "Apple"}; // массив с названиями компаний
+
+        List<Employee> employees = new ArrayList<>(num);
+
+        // добавление num объектов Employee в список (employees)
+        for (int i = 0; i < num; i++) {
+            // TODO: объекты создавать с рандомными значениями. Возраст от 21 до 60 и не забудьте про зп
+            employees.add(new Employee(   ));
+        }
+        return employees;
+    }
+}
---
+++ src/ru/itmo/lessons/lesson14/hw/MessageTask.java
@@ -0,0 +1,39 @@
+package ru.itmo.lessons.lesson14.hw;
+
+import java.util.*;
+
+public class MessageTask {
+    public static void countEachPriority(List<Message> messageList) {
+        // TODO:  Подсчитать количество сообщений для каждого приоритета
+        //  Ответ в консоль
+    }
+
+    public static void countEachCode(List<Message> messageList) {
+        // TODO: Подсчитать количество сообщений для каждого кода сообщения
+        //  Ответ в консоль
+    }
+
+    public static void uniqueMessageCount(List<Message> messageList) {
+        // TODO: Подсчитать количество уникальных сообщений
+        //  Ответ в консоль
+    }
+
+    public static List<Message> uniqueMessagesInOriginalOrder(List<Message> messageList) {
+        // TODO: вернуть только неповторяющиеся сообщения и в том порядке,
+        //  в котором они встретились в первоначальном списке
+        //  Например, было: [{URGENT, 4}, {HIGH, 9}, {LOW, 3}, {HIGH, 9}]
+        //  на выходе: [{URGENT, 4}, {HIGH, 9}, {LOW, 3}]
+        return null;
+    }
+
+    public static void removeEach(List<Message> messageList, MessagePriority priority) {
+        // TODO: удалить из коллекции каждое сообщение с заданным приоритетом
+        //  вывод в консоль до удаления и после удаления
+    }
+
+    public static void removeOther(List<Message> messageList, MessagePriority priority) {
+        // TODO: удалить из коллекции все сообщения, кроме тех, которые имеют заданный приоритет
+        //  вывод в консоль до удаления и после удаления
+    }
+
+}
